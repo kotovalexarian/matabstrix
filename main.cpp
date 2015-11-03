8ff7ad3dfb56f03b3b618d6d2fc0d695100de984
Meoweg <meoweg@users.noreply.github.com>
2015-11-03 15:40:20 +0000

    Add basic GLFW initialization

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+#include <cstdlib>
+
+#include <GL/glfw.h>
+
+#include <emscripten/emscripten.h>
+
+static void iterate();
+
 int main()
 {
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+
+  if (!glfwOpenWindow(640, 480, 8, 8, 8, 8, 16, 0, GLFW_WINDOW))
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  emscripten_set_main_loop(iterate, 60, 1);
+}
+
+void iterate()
+{
+  glfwPollEvents();
 }
