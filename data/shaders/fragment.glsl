1bfeb9e99da0a73a52a0eb0ae60aa1f57b2dabb3
Meoweg <meoweg@users.noreply.github.com>
2015-11-07 15:26:38 +0000

    Add hardcoded light

--- data/shaders/fragment.glsl
+++ data/shaders/fragment.glsl
@@ -1,10 +1,12 @@
 precision lowp float;
 
 varying vec2 f_tex_coord;
+varying float light_weight;
 
 uniform sampler2D texture;
 
 void main(void) {
   vec2 flipped_tex_coord = vec2(f_tex_coord.x, 1.0 - f_tex_coord.y);
-  gl_FragColor = texture2D(texture, flipped_tex_coord);
+  vec4 texture_color = texture2D(texture, flipped_tex_coord);
+  gl_FragColor = vec4(texture_color.rgb * light_weight, texture_color.a);
 }
--- data/shaders/vertex.glsl
+++ data/shaders/vertex.glsl
@@ -1,11 +1,14 @@
 attribute vec4 position;
 attribute vec2 tex_coord;
+attribute vec3 normal;
 
 varying vec2 f_tex_coord;
+varying float light_weight;
 
 uniform mat4 mvp;
 
 void main(void) {
   gl_Position = mvp * position;
   f_tex_coord = tex_coord;
+  light_weight = max(dot(normal, vec3(0.0, 0.0, 1.0)), 0.0);
 }
