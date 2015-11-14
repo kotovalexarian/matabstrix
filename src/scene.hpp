6ac5a769afdc91bb1bbdfcf7b084090304ec6b2c
Meoweg <meoweg@users.noreply.github.com>
2015-11-14 08:04:15 +0000

    Add sun to scene

--- src/scene.hpp
+++ src/scene.hpp
@@ -2,6 +2,7 @@
 #define _SCENE_HPP_
 
 #include "object.hpp"
+#include "light/sun.hpp"
 
 #include <vector>
 
@@ -9,20 +10,34 @@
 
 struct Scene
 {
-  inline Scene &operator <<(const Object *object);
+  inline Scene &operator<<(const Object *object);
+  inline Scene &operator<<(const Lights::Sun *sun);
+
+  inline const Lights::Sun *sun() const;
 
   inline void draw(const glm::mat4 &mvp) const;
 
 private:
   std::vector<const Object*> _objects;
+  const Lights::Sun *_sun = nullptr;
 };
 
-Scene &Scene::operator <<(const Object *object)
+Scene &Scene::operator<<(const Object *object)
 {
   _objects.push_back(object);
-
   return *this;
 }
+
+Scene &Scene::operator<<(const Lights::Sun *sun)
+{
+  _sun = sun;
+  return *this;
+}
+
+const Lights::Sun *Scene::sun() const
+{
+  return _sun;
+};
 
 void Scene::draw(const glm::mat4 &mvp) const
 {
